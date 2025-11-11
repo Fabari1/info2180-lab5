57c40932f48f1bdfa01748ae5c488e1c84e1e862
Fabari Williams <131101653+Fabari1@users.noreply.github.com>
2025-11-11 12:31:46 -0500

    Implement country lookup functionality in world.js and enhance world.php for country-specific queries

---
+++ world.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+    var countryInput;
+
+    document.getElementById("lookup").onclick = function(event) {
+
+        event.preventDefault();
+
+        countryInput = document.getElementById("country").value.trim().replace(/(<\/?[^>]+>)/gi, "");
+        console.log("Country input: " + countryInput);
+        const httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function() {
+            if (httpRequest.readyState === XMLHttpRequest.DONE) {
+                if (httpRequest.status === 200) {
+                document.getElementById("result").innerHTML = httpRequest.responseText;
+                } else {
+                document.getElementById("result").innerHTML = "There was a problem with the request.";
+                }
+            }
+        };
+        httpRequest.open("GET", "http://localhost/info2180-lab5/world.php?country=" + encodeURIComponent(countryInput));
+        httpRequest.send();
+    };
+
+
+}
--- world.php
+++ world.php
@@ -1,15 +1,29 @@
 <?php
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
+$country = $_GET['country'] ?? '';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+if (count($results) === 0) {
+    echo "<p style='color: red;'>No countries found matching '" . htmlspecialchars($country, ENT_QUOTES, 'UTF-8') . "'</p>";
+    exit;
+}else {
+    echo "<p>Found " . count($results) . " matching '" . htmlspecialchars($country, ENT_QUOTES, 'UTF-8') . "':</p>";
+}
+
 ?>
+
 <ul>
 <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
